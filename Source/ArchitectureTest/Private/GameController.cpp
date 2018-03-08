839d7a92d0e7a9a1e371d96d9a460e43d39e6588
Jendoliver <alvarezmonllor@gmail.com>
2018-03-08 20:15:08 +0100

    Started to implement Team logics

--- Source/ArchitectureTest/Private/GameController.cpp
+++ Source/ArchitectureTest/Private/GameController.cpp
@@ -1,7 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "GameController.h"
+#include "GameplayTagContainer.h"
 
+ETeam AGameController::GetTeam()
+{
+	return Team;
+}
 
-
-
+void AGameController::SetTeam(ETeam NewTeam)
+{
+	Team = NewTeam;
+}
--- Source/ArchitectureTest/Public/GameController.h
+++ Source/ArchitectureTest/Public/GameController.h
@@ -6,15 +6,31 @@
 #include "GameFramework/Controller.h"
 #include "GameController.generated.h"
 
-//class FGameplayTagContainer;
+UENUM( BlueprintType )
+enum class ETeam : uint8
+{
+	Red		UMETA( DisplayName="Red Team" ),
+	Blue	UMETA( DisplayName="Blue Team" )
+	// TODO implementar Neutral y mover a otra clase para utilizarlo en CapturableVolume
+};
 
 /**
  * 
  */
-UCLASS()
+UCLASS( Blueprintable )
 class ARCHITECTURETEST_API AGameController : public AController
 {
 	GENERATED_BODY()
 
-	// TODO implement FGameplayTagContainer Team;
+private:
+	UPROPERTY( EditAnywhere )
+	ETeam Team;
+
+public:
+	UFUNCTION( BlueprintCallable )
+	ETeam GetTeam();
+
+	UFUNCTION( BlueprintCallable )
+	void SetTeam(ETeam NewTeam);
+	
 };
