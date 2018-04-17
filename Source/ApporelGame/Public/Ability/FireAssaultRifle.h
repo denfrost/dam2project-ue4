48b4609d4518e79ee247cfdec3626b1dd8b970c6
Raikish <albert.montagut.casero@gmail.com>
2018-04-17 10:41:45 +0200

    solving conflicts

---
+++ Source/ApporelGame/Public/Ability/FireAssaultRifle.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ability.h"
+#include "FireAssaultRifle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APPORELGAME_API AFireAssaultRifle : public AAbility
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
