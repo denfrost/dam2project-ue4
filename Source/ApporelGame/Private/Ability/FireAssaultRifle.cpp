29c388f054ac4a817df170b36c9577ba3b32c474
Raikish <albert.montagut.casero@gmail.com>
2018-04-17 12:53:34 +0200

    Assaultl Rifle now shots, with no effects, so you can't se anything!  (Adding some effect to implent in the next commit)

--- Source/ApporelGame/Private/Ability/FireAssaultRifle.cpp
+++ Source/ApporelGame/Private/Ability/FireAssaultRifle.cpp
@@ -2,6 +2,7 @@
 
 #include "FireAssaultRifle.h"
 #include "ArenaCharacter.h"
+#include "AssaultRifle.h"
 #include "DrawDebugHelpers.h"
 #include "Kismet/GameplayStatics.h"
 
@@ -9,16 +10,27 @@
 
 void AFireAssaultRifle::ExecuteAbility_Implementation(AActor* executor)
 {
-	UE_LOG(LogTemp, Warning, TEXT("executor name : %s"), *executor->GetName());
+
+	UWorld* WorldContext = executor->GetWorld();
+	if (!WorldContext)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("NO World Context Available!"));
+		return;
+	}
 
 	auto* Character = Cast<AArenaCharacter>(executor);
+	if (!Character)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No Pawn"));
+		return;
+	}
 
 	auto* AsssaultRiffle = Character->GetWeapon();
-	if (AsssaultRiffle)
+	if (!AsssaultRiffle)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("executor name : %s"), AsssaultRiffle);
-		
-	}*/
+		UE_LOG(LogTemp, Warning, TEXT("No weapon"));
+		return;
+	}
 
 	FVector EyeLocation;
 	FRotator EyeRotation;
@@ -29,10 +41,10 @@ void AFireAssaultRifle::ExecuteAbility_Implementation(AActor* executor)
 	FVector ShotDirection = EyeRotation.Vector();
 	FVector TraceEnd = EyeLocation + (ShotDirection * 10000);
 
-	//Ajustando la collision para que no choque contra el mismo ni el propietario
+	//Ajustando la collision para que no choque contra el pawn ni el arma
 	FCollisionQueryParams QueryParams;
 	QueryParams.AddIgnoredActor(executor);
-	QueryParams.AddIgnoredActor(this);
+	QueryParams.AddIgnoredActor(AsssaultRiffle);
 	QueryParams.bTraceComplex = true;
 
 	//Struct que contiene todos los datos del impacto
@@ -41,19 +53,21 @@ void AFireAssaultRifle::ExecuteAbility_Implementation(AActor* executor)
 	//Partile "Target" parameter
 	FVector TracerEndPoint = TraceEnd;
 
-	//El siguiente metodo traza una linea desde el punto de vista del propietario del arma hasta 10 metros en esa direccion.
+	//El siguiente metodo traza una linea desde el punto de vista del propietario del arma hasta 100 metros en esa direccion.
 	//Se le pasa el struct hit que sera llenado en caso de encontrar un actor con los parametros de condicion especificados
 	//en el query params y retornara true si ha encontrado dicho actor.
-	if (GetWorld()->LineTraceSingleByChannel(Hit, EyeLocation, TraceEnd, ECC_Visibility, QueryParams))
+	if (WorldContext->LineTraceSingleByChannel(Hit, EyeLocation, TraceEnd, ECC_Visibility, QueryParams))
 	{
 		//Blocking hit! Process Damage
 
 		AActor* HitActor = Hit.GetActor();
+		UE_LOG(LogTemp, Warning, TEXT("HitActor name : %s"), *HitActor->GetName());
+
 
 		UGameplayStatics::ApplyPointDamage(HitActor, 20.0f, ShotDirection, Hit, executor->GetInstigatorController(), this, DamageType);
 
 		TracerEndPoint = Hit.ImpactPoint;
 	}
-	DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::White, false, 1.0f, 0, 1.0f);
+	DrawDebugLine( WorldContext, EyeLocation, TraceEnd, FColor::Yellow, false, 3.0f, 0, 3.0f);
 
 }
