a9b3f39dd4c39c5cad679802a1c6cbe863033ee3
Raikish <albert.montagut.casero@gmail.com>
2018-05-23 22:09:23 +0200

    Changing BP_GameMode parent class, new methods to Game Mode: Increment Score, Get Score Reset Score

--- Source/ApporelGame/ApporelGameGameModeBase.cpp
+++ Source/ApporelGame/ApporelGameGameModeBase.cpp
@@ -1,7 +1,39 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ApporelGameGameModeBase.h"
+#include "TeamUtils.h"
 
+float AApporelGameGameModeBase::GetScoreTeam(ETeam Team)
+{
+	return (Team == ETeam::Blue) ? ScoreTeamBlue : ScoreTeamRed;
+}
 
+void AApporelGameGameModeBase::IncrementScore(ETeam Team, float Score)
+{
+	switch (Team)
+	{
+	case ETeam::Blue:
+		ScoreTeamBlue += Score;
+		break;
+	case ETeam::Red:
+		ScoreTeamRed += Score;
+		break;
+	default:
+		break;
+	}
+}
 
-
+void AApporelGameGameModeBase::ResetScore(ETeam Team)
+{
+	switch (Team)
+	{
+	case ETeam::Blue:
+		ScoreTeamBlue = 0;
+		break;
+	case ETeam::Red:
+		ScoreTeamRed = 0;
+		break;
+	default:
+		break;
+	}
+}
--- Source/ApporelGame/ApporelGameGameModeBase.h
+++ Source/ApporelGame/ApporelGameGameModeBase.h
@@ -14,7 +14,17 @@ class APPORELGAME_API AApporelGameGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 	
-	
-	
-	
+protected:
+	float ScoreTeamBlue = 0.f; // Create Team class (?)
+	float ScoreTeamRed = 0.f; 
+
+public:
+	UFUNCTION(BlueprintCallable)
+	float GetScoreTeam(ETeam Team);
+
+	UFUNCTION(BlueprintCallable)
+	void IncrementScore(ETeam Team, float Score);
+
+	UFUNCTION(BlueprintCallable)
+	void ResetScore(ETeam Team);
 };
